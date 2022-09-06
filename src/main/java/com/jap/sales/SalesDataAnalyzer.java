10a8bbdd0a7d6ec4cd6ba44033e4d81f85a06dec
rishavemail786 <rishavemail786@gmail.com>
2022-09-06 17:50:39 +0530

    SOLUTION DONE

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -1,6 +1,7 @@
 package com.jap.sales;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
@@ -12,14 +13,40 @@ import java.util.List;
 
 public class SalesDataAnalyzer {
    public List<SalesRecord> readFile(String fileName) {
-       return null;
+       List<SalesRecord> recordList =new ArrayList<>();
+       try {
+           FileReader fileReader =new FileReader(fileName);
+           BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String line=bufferedReader.readLine();
+            while ((line=bufferedReader.readLine())!=null){
+                String[] strings =line.split(",");
+                String date=strings[0];
+                int   customer_id=Integer.parseInt(strings[1]);
+                int   product_category=Integer.parseInt(strings[2]);
+                String payment_method=strings[3];
+                 double amount=Double.parseDouble(strings[4]);
+                 double time_on_site=Double.parseDouble(strings[5]);
+                 int clicks_in_site=Integer.parseInt(strings[6]);
+
+                 recordList.add(new SalesRecord(date,customer_id,product_category,payment_method,amount,time_on_site,clicks_in_site));
+            }
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+
+       return recordList;
     }
 
     public List<SalesRecord> getAllCustomersSortedByPurchaseAmount(List<SalesRecord> salesData){
-      return null;
+       salesData.sort((o1, o2) -> (int)(o2.getAmount()-o1.getAmount()));
+      return salesData;
     }
     public SalesRecord getTopCustomerWhoSpentMaxTimeOnSite(List<SalesRecord> salesData){
-       return null;
+       salesData.sort((o1, o2) -> (int)(o2.getTime_on_site()-o1.getTime_on_site()));
+       return salesData.get(0);
     }
 
 
